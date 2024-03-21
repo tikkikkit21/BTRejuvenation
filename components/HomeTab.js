a56884a98c21611a5d1c3c36e45d9a64f7f0621c
tikkikkit21 <tikkikkit21@gmail.com>
2024-03-21 10:36:35 -0400

    Center and zoom map

--- components/HomeTab.js
+++ components/HomeTab.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import { View, Text, Dimensions } from 'react-native';
+import { View } from 'react-native';
 import MapView from 'react-native-maps';
 import styles from '../styles/App.style';
-
-let { height, width } = Dimensions.get('window')
 
 function HomeTab() {
     return (
         <View style={styles.container}>
-            {/* <Text>This is the Home Tab</Text> */}
             <MapView
                 style={styles.map}
                 initialRegion={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
+                    latitude: 37.227613,
+                    longitude: -80.422137,
+                    latitudeDelta: 0.0122,
+                    longitudeDelta: 0.0121,
                 }}
             />
         </View>
