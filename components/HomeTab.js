2a726701bc161333992c51dd39b8d4689a5fbee9
tikkikkit21 <tikkikkit21@gmail.com>
2024-03-20 20:43:40 -0400

    Add home tab component

---
+++ components/HomeTab.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+function HomeTab() {
+    return (
+        <View>
+            <Text>This is the Home Tab</Text>
+        </View>
+    )
+}
+
+export default HomeTab
