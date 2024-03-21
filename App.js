f27f44e0b6fee763c0d86fddd5fda2de054c098a
tikkikkit21 <tikkikkit21@gmail.com>
2024-03-20 21:45:31 -0400

    Add icons to tabs

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { Ionicons, FontAwesome5, Foundation } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeTab from './components/HomeTab';
@@ -12,7 +13,36 @@ const Tab = createBottomTabNavigator();
 export default function App() {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let icon;
+
+                        switch (route.name) {
+                            case "Home":
+                                icon = <Ionicons name='map' size={size} color={color} />
+                                break;
+                            case "Alerts":
+                                icon = <Foundation name='alert' size={size} color={color} />
+                                break;
+                            case "Plan a Trip":
+                                icon = <FontAwesome5 name='map-marked-alt' size={size} color={color} />
+                                break;
+                            case "Routes":
+                                icon = <FontAwesome5 name='route' size={size} color={color} />
+                                break;
+
+                            default:
+                                console.log("Unknown name:", route.name)
+                                break;
+                        }
+
+                        return icon;
+                    },
+                    tabBarActiveTintColor: 'tomato',
+                    tabBarInactiveTintColor: 'gray',
+                })}
+            >
                 <Tab.Screen name="Home" component={HomeTab} />
                 <Tab.Screen name="Alerts" component={AlertsTab} />
                 <Tab.Screen name="Plan a Trip" component={PlanTrip} />
