554195b31624011c187b858fd7e7aeb3f37d6f49
tikkikkit21 <tikkikkit21@gmail.com>
2024-03-20 21:22:04 -0400

    Add alerts tab

---
+++ components/AlertsTab.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+function AlertsTab() {
+    return (
+        <View>
+            <Text>This is the Alerts Tab</Text>
+        </View>
+    )
+}
+
+export default AlertsTab;
