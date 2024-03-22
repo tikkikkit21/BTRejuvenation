28a69ed14050ea85072ea8c34b8df6348870656d
tikkikkit21 <tikkikkit21@gmail.com>
2024-03-22 09:05:31 -0400

    Add marker to map

--- components/HomeTab.js
+++ components/HomeTab.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { View } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import appStyles from '../styles/App.style';
 import styles from '../styles/HomeTab.style';
-import { getBus } from '../backend/busController';
 
 function HomeTab() {
-    getBus("HWD");
     return (
         <View style={appStyles.container}>
             <MapView
@@ -18,7 +16,16 @@ function HomeTab() {
                     longitudeDelta: 0.0121,
                 }}
                 showsUserLocation={true}
-            />
+            >
+                <Marker
+                    coordinate={{
+                        latitude: 37.22578,
+                        longitude: -80.443656
+                    }}
+                    title="HWD"
+                    description="Sample description"
+                />
+            </MapView>
         </View>
     )
 }
