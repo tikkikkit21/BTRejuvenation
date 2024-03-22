7184b28014949b7aa2e63c8fc43b947368e278a8
tikkikkit21 <tikkikkit21@gmail.com>
2024-03-21 21:56:46 -0400

    Implement method to fetch all buses

--- backend/busController.js
+++ backend/busController.js
@@ -1,3 +1,9 @@
-export function getAllBuses() {
-    
+import axios from "axios";
+
+const ROOT = "http://www.bt4uclassic.org/webservices/bt4u_webservice.asmx";
+
+export async function getAllBuses() {
+    console.log("PATH:", `${ROOT}/GetCurrentBusInfo`)
+    const { data } = await axios.get(`${ROOT}/GetCurrentBusInfo`)
+    console.log(data)
 }
