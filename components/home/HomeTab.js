fdae2aac81352fcfe46742d9bce750965c2d9596
tikkikkit21 <tikkikkit21@gmail.com>
2024-03-22 11:40:31 -0400

    Create stack navigator

--- components/home/HomeTab.js
+++ components/home/HomeTab.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
 import Map from './Map';
+import FeedbackForm from './FeedbackForm';
+
+const Stack = createStackNavigator();
 
 function HomeTab() {
     return (
-        <Map />
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false
+            }}
+            initialRouteName="Map"
+        >
+            <Stack.Screen name="Map" component={Map} />
+            <Stack.Screen name="FeedbackForm" component={FeedbackForm} />
+        </Stack.Navigator>
     );
 }
 
