3c1dec2263d334a3c7959cae2c96d66489f886ca
tikkikkit21 <tikkikkit21@gmail.com>
2024-03-21 22:34:58 -0400

    Fetch all buses

--- backend/busController.js
+++ backend/busController.js
@@ -4,9 +4,10 @@ import { xml2js } from "xml-js";
 const ROOT = "http://www.bt4uclassic.org/webservices/bt4u_webservice.asmx";
 
 export async function getAllBuses() {
-    console.log("PATH:", `${ROOT}/GetCurrentBusInfo`)
-    const { data } = await axios.get(`${ROOT}/GetCurrentBusInfo`)
-    json = xml2js(data, {compact: true})
-    json = json.DocumentElement.LatestInfoTable
-    console.log(json)
+    const { data } = await axios.get(`${ROOT}/GetCurrentBusInfo`);
+    json = xml2js(data, { compact: true });
+    json = json.DocumentElement.LatestInfoTable;
+
+    console.log(json);
+    return json;
 }
