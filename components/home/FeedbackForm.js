567689d21cdd2fbfceb0f9864720372a01cb6ca8
tikkikkit21 <tikkikkit21@gmail.com>
2024-03-22 15:52:21 -0400

    Create initial input form

--- components/home/FeedbackForm.js
+++ components/home/FeedbackForm.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import styles from '../../styles/App.style';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 function FeedbackForm() {
     return (
         <View style={styles.container}>
-            <Text>This is the Feedback form Tab</Text>
+            <Text style={styles.header}>Feedback</Text>
+            <TextInput placeholder="Placeholder text" style={styles.input} />
         </View>
     )
 }
 
 export default FeedbackForm;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    header: {
+        fontSize: 24
+    },
+    input: {
+        padding: 5,
+        borderColor: '#000',
+        borderWidth: 1,
+        borderRadius: 5
+    }
+});
