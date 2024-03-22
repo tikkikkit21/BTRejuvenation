30bc66b5a0ffa3fce8c8e5dadabf666eebf5ab58
tikkikkit21 <tikkikkit21@gmail.com>
2024-03-21 22:24:04 -0400

    Convert to JS object and trim data

--- backend/busController.js
+++ backend/busController.js
@@ -1,11 +1,12 @@
 import axios from "axios";
-import { xml2json } from "xml-js";
+import { xml2js } from "xml-js";
 
 const ROOT = "http://www.bt4uclassic.org/webservices/bt4u_webservice.asmx";
 
 export async function getAllBuses() {
     console.log("PATH:", `${ROOT}/GetCurrentBusInfo`)
     const { data } = await axios.get(`${ROOT}/GetCurrentBusInfo`)
-    json = xml2json(data, {compact: true})
+    json = xml2js(data, {compact: true})
+    json = json.DocumentElement.LatestInfoTable
     console.log(json)
 }
