3619393bffac9ca375b61d7a97c130a0a09fc986
Connor Roth <82622545+connorroth20@users.noreply.github.com>
2024-03-22 15:30:51 -0400

    Manual refresh button (#4)
    Co-authored-by: tikkikkit21 <tikkikkit21@gmail.com>

--- components/HomeTab.js
+++ components/HomeTab.js
@@ -1,20 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { View, TouchableOpacity } from 'react-native';
 
 import MapView, { Marker } from 'react-native-maps';
 import { getAllBuses } from '../backend/busController';
-import { FontAwesome6 } from '@expo/vector-icons';
+import { FontAwesome, FontAwesome6 } from '@expo/vector-icons';
 
 import appStyles from '../styles/App.style';
 import styles from '../styles/HomeTab.style';
 
 function HomeTab() {
     const [buses, setBuses] = useState([]);
+    const [isOnCooldown, setIsOnCooldown] = useState(false);
+    const refreshTimer = useRef(null);
 
     useEffect(() => {
         loadBuses();
-        setInterval(loadBuses, 10000);
+        refreshTimer.current = setInterval(() => {
+            loadBuses();
+        }, 10000);
+
+        return () => {
+            clearInterval(refreshTimer.current);
+        };
     }, []);
+
+    const handleButtonClick = () => {
+        if (!isOnCooldown) {
+            clearInterval(refreshTimer.current);
+            refreshTimer.current = setInterval(loadBuses, 10000);
+
+            loadBuses();
+
+            setIsOnCooldown(true);
+            setTimeout(() => {
+                setIsOnCooldown(false);
+            }, 5000);
+        }
+    }
 
     async function loadBuses() {
         const buses = await getAllBuses();
@@ -35,8 +57,13 @@ function HomeTab() {
             >
                 {getMarkers(buses)}
             </MapView>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity onPress={handleButtonClick}>
+                    <FontAwesome name="refresh" size={24} color="white" />
+                </TouchableOpacity>
+            </View>
         </View>
-    )
+    );
 }
 
 function getMarkers(buses) {
