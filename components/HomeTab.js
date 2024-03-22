d7784f6c31c67dc7e71a50359c20dd65db5d7029
tikkikkit21 <tikkikkit21@gmail.com>
2024-03-22 09:16:34 -0400

    Display all buses on map

--- components/HomeTab.js
+++ components/HomeTab.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import appStyles from '../styles/App.style';
 import styles from '../styles/HomeTab.style';
+import { getAllBuses } from '../backend/busController';
 
 function HomeTab() {
+    const [buses, setBuses] = useState([]);
+
+    async function getBuses() {
+        const buses = await getAllBuses();
+        setBuses(buses);
+    }
+
+    useEffect(() => {
+        getBuses();
+    }, []);
+
     return (
         <View style={appStyles.container}>
             <MapView
@@ -17,17 +29,25 @@ function HomeTab() {
                 }}
                 showsUserLocation={true}
             >
-                <Marker
-                    coordinate={{
-                        latitude: 37.22578,
-                        longitude: -80.443656
-                    }}
-                    title="HWD"
-                    description="Sample description"
-                />
+                {getMarkers(buses)}
             </MapView>
         </View>
     )
+}
+
+function getMarkers(buses) {
+    return buses.map(busObj => {
+        return (
+            <Marker
+                coordinate={{
+                    latitude: busObj.Latitude,
+                    longitude: busObj.Longitude
+                }}
+                title={busObj.RouteShortName}
+                description={`Last stop: ${busObj.LastStopName}`}
+            />
+        )
+    });
 }
 
 export default HomeTab;
