562c2fbf6c2b1ebdbef4a8ada41e802662b8b16e
Tikki <35748816+tikkikkit21@users.noreply.github.com>
2024-03-22 09:39:32 -0400

    Add maps to home screen (#3)
    Added maps to the home tab. This will provide a foundation for rending bus locations and route pathing.

--- components/HomeTab.js
+++ components/HomeTab.js
@@ -1,10 +1,22 @@
-import { View, Text } from 'react-native';
-import styles from '../styles/App.style'
+import React from 'react';
+import { View } from 'react-native';
+import MapView from 'react-native-maps';
+import appStyles from '../styles/App.style';
+import styles from '../styles/HomeTab.style'
 
 function HomeTab() {
     return (
-        <View style={styles.container}>
-            <Text>This is the Home Tab</Text>
+        <View style={appStyles.container}>
+            <MapView
+                style={styles.map}
+                initialRegion={{
+                    latitude: 37.227613,
+                    longitude: -80.422137,
+                    latitudeDelta: 0.0122,
+                    longitudeDelta: 0.0121,
+                }}
+                showsUserLocation={true}
+            />
         </View>
     )
 }
