27c1ab57218f2b9a54563f120555314be456e379
tikkikkit21 <tikkikkit21@gmail.com>
2024-03-22 11:40:22 -0400

    Create screen for feedback form

---
+++ components/home/FeedbackForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from '../../styles/App.style';
+
+function FeedbackForm() {
+    return (
+        <View style={styles.container}>
+            <Text>This is the Alerts Tab</Text>
+        </View>
+    )
+}
+
+export default FeedbackForm;
