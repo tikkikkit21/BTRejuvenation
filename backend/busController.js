c2226a540c4c15188a3bd9871e7f9a28d0bfb7b7
tikkikkit21 <tikkikkit21@gmail.com>
2024-03-21 22:21:03 -0400

    Convert XML to JSON

--- backend/busController.js
+++ backend/busController.js
@@ -1,9 +1,11 @@
 import axios from "axios";
+import { xml2json } from "xml-js";
 
 const ROOT = "http://www.bt4uclassic.org/webservices/bt4u_webservice.asmx";
 
 export async function getAllBuses() {
     console.log("PATH:", `${ROOT}/GetCurrentBusInfo`)
     const { data } = await axios.get(`${ROOT}/GetCurrentBusInfo`)
-    console.log(data)
+    json = xml2json(data, {compact: true})
+    console.log(json)
 }
