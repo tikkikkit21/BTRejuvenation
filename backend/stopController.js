649bdea5794d4023fd35e7f469c0ee5632092408
tikkikkit21 <tikkikkit21@gmail.com>
2024-03-23 10:02:06 -0400

    Create controller for stops

---
+++ backend/stopController.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+import { xml2js } from "xml-js";
+
+const ROOT = "http://www.bt4uclassic.org/webservices/bt4u_webservice.asmx";
+
+export async function getStops(busCode) {
+
+}
