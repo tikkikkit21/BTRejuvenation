c2e65b16f26c9ffb6312bd648aa8fcecc765394b
samuelparada6 <samuelparada6@vt.edu>
2024-03-25 13:00:27 -0400

    added packages for camera, QR code for react-native & dummy code to implement QR code in FeedbackForm.js

--- components/home/FeedbackForm.js
+++ components/home/FeedbackForm.js
@@ -1,7 +1,10 @@
 import { FontAwesome } from '@expo/vector-icons';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, TouchableOpacity } from 'react-native';
 import Slider from '@react-native-community/slider';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import { RNCamera } from 'react-native-camera';
+import { request, PERMISSIONS } from 'react-native-permissions';
 import { submitFeedback } from '../../backend/feedbackController';
 
 
@@ -27,13 +30,35 @@ function FeedbackForm() {
             alert(`Full Name: ${fullName}\nRating:${sliderValue}\nComments: ${comments}`);
         }  
     };
+    // Successful reading QR code
+    onSuccess = e => {
+        Linking.openURL(e.data).catch(err =>
+          console.error('An error occured', err)
+        );
+      };
 
     return (
         <ScrollView contentContainerStyle={styles.container}>
             <Text style={styles.title}>Feedback Form</Text>
             <Text style={styles.question}>Scan QR Code on the Bus?</Text>
             <View style={styles.buttonContainer}>
-                <Button title="Scan" onPress={() => alert('Form not linked.')} />
+                {/* <Button title="Scan" onPress={() => alert('Form not linked.')} /> */}
+                <QRCodeScanner
+                    onRead={this.onSuccess}
+                    flashMode={RNCamera.Constants.FlashMode.torch}
+                    topContent={
+                    <Text style={styles.centerText}>
+                        Go to{' '}
+                        <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+                        your computer and scan the QR code.
+                    </Text>
+                    }
+                    bottomContent={
+                    <TouchableOpacity style={styles.buttonTouchable}>
+                        <Text style={styles.buttonText}>OK. Got it!</Text>
+                    </TouchableOpacity>
+                    }
+                />
             </View>
             {getQuestions(fullName, setFullName, comments, setComments, sliderValue, setSliderValue)}
             <View style={styles.submitContainer}>
