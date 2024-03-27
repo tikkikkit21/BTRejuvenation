8d1e06e443a98b13ab73d8f0d39fa5999f995959
tikkikkit21 <tikkikkit21@gmail.com>
2024-03-26 22:42:40 -0400

    Remove dead code

--- backend/routeController.js
+++ backend/routeController.js
@@ -3,34 +3,29 @@ import { xml2js } from "xml-js";
 
 const ROOT = "http://www.bt4uclassic.org/webservices/bt4u_webservice.asmx";
 
-export async function getScheduledRoutes(stopCode){
+export async function getScheduledRoutes(stopCode) {
     // stopcode should be a number 1000 -> 2303
     // leaving servicedate as blank because it auto-searches for today's date
     const { data } = await axios.get((`${ROOT}/GetScheduledRoutes?stopCode=${stopCode}&serviceDate=`));
     json = xml2js(data, { compact: true });
     scheduledStops = json.DocumentElement.ScheduledRoutes;
-    
+
     //when size is 1, data will be formatted as {}, when we need it as [] for mapping
-    if(Array.isArray(scheduledStops)){
+    if (Array.isArray(scheduledStops)) {
         busRoutes = scheduledStops.map(route => [route.RouteName._text, route.RouteColor._text, route.RouteShortName._text, route.RouteTextColor._text, route.RouteURL._text, route.ServiceLevel._text]);
     }
-    else{
+    else {
         busRoutes = [[scheduledStops.RouteName._text, scheduledStops.RouteColor._text, scheduledStops.RouteShortName._text, scheduledStops.RouteTextColor._text, scheduledStops.RouteURL._text, scheduledStops.ServiceLevel._text]];
     }
-    //const busRoutes = formatScheduledStops(scheduledStops)
-    //console.log(busRoutes);
+
     return busRoutes;
 }
 
 export async function getAllStops() {
     const { data } = await axios.get(`${ROOT}/GetScheduledStopCodes?routeShortName=`);
-    //console.log(data);
     json = xml2js(data, { compact: true });
     scheduledStops = json.DocumentElement.ScheduledStops;
-    const stops = scheduledStops.map(stop =>  [stop.StopCode._text, stop.StopName._text]);
-    //console.log(stops)
-    //return a list of [stopCode, stopName]
-    //console.log(stops)
+    const stops = scheduledStops.map(stop => [stop.StopCode._text, stop.StopName._text]);
+
     return stops;
 }
-
