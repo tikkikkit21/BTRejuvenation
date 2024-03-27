0d5e30cfd041a88e777e4c2eff43571931b16beb
tikkikkit21 <tikkikkit21@gmail.com>
2024-03-26 22:52:15 -0400

    Format files

--- components/routes/RoutesTab.js
+++ components/routes/RoutesTab.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList} from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 import styles from '../styles/Route.style';
 import { getAllStops, getScheduledRoutes } from '../backend/routeController';
@@ -11,70 +11,70 @@ function RouteTab() {
     const [stops, setStops] = useState([]);
     const [routes, setRoutes] = useState([]);
     const [selectedStop, selectStop] = useState("");
-   useEffect(() => {
-    async function fetchStops() {
-        try {
-            const stopLocal = await getAllStops();
-            setStops(stopLocal);
-        } catch (error) {
-            console.error('Error fetching stops:', error);
+    useEffect(() => {
+        async function fetchStops() {
+            try {
+                const stopLocal = await getAllStops();
+                setStops(stopLocal);
+            } catch (error) {
+                console.error('Error fetching stops:', error);
+            }
         }
-    }
-   
-    fetchStops();
 
-    async function fetchAllRoutes(){
-        try {
-            const routeLocal = await getScheduledRoutes("");
-            setRoutes(routeLocal);
-        } catch (error) {
-            console.error('Error fetching stops:', error);
+        fetchStops();
+
+        async function fetchAllRoutes() {
+            try {
+                const routeLocal = await getScheduledRoutes("");
+                setRoutes(routeLocal);
+            } catch (error) {
+                console.error('Error fetching stops:', error);
+            }
         }
-    }
 
-    fetchAllRoutes()
-   }, []);
+        fetchAllRoutes()
+    }, []);
 
-   
-   const handleStopChange = (itemValue) => {    
-    selectStop(itemValue);
-    stopCode = itemValue.value
 
-    async function fetchScheduledRoutes() {
-        try {
-            const routesLocal = await getScheduledRoutes(stopCode);
-            setRoutes(routesLocal)
-        } catch (error) {
-            console.error('Error fetching stops:', error);
+    const handleStopChange = (itemValue) => {
+        selectStop(itemValue);
+        stopCode = itemValue.value
+
+        async function fetchScheduledRoutes() {
+            try {
+                const routesLocal = await getScheduledRoutes(stopCode);
+                setRoutes(routesLocal)
+            } catch (error) {
+                console.error('Error fetching stops:', error);
+            }
         }
-    }
-   
-    fetchScheduledRoutes();
 
-};
+        fetchScheduledRoutes();
+
+    };
     return (
         <View style={styles.container}>
             <DropDownPicker
                 items={stops.map(stop => ({ label: stop[1], value: stop[0] }))}
                 defaultValue={selectedStop}
                 placeholder="--Filter By Stop--"
-                value = {selectedStop}
+                value={selectedStop}
                 containerStyle={{ height: 50, width: '100%' }}
                 style={styles.picker}
                 dropDownStyle={{ backgroundColor: '#fafafa' }}
                 onSelectItem={handleStopChange}
-                search = {true}
+                search={true}
                 open={open}
                 setOpen={setOpen}
             />
-            <Text style = {{fontSize: 27,color: '#1E1E1E'}}>Scheduled Routes:</Text>
+            <Text style={{ fontSize: 27, color: '#1E1E1E' }}>Scheduled Routes:</Text>
             <FlatList
                 data={routes}
                 keyExtractor={(item, index) => index.toString()}
                 renderItem={({ item }) => (
-                    <View style = {styles.flatListItem}>
-                        <Text style = {{fontSize: 20, color: '#' + item[1], textAlign: 'left'}}>{item[2]}</Text>
-                        <Text style = {{fontSize: 22, color: '#' + item[1], fontWeight: 'bold'}}>{item[0]}</Text>
+                    <View style={styles.flatListItem}>
+                        <Text style={{ fontSize: 20, color: '#' + item[1], textAlign: 'left' }}>{item[2]}</Text>
+                        <Text style={{ fontSize: 22, color: '#' + item[1], fontWeight: 'bold' }}>{item[0]}</Text>
 
                     </View>
                 )}
