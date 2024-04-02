f36b3e56e382e6a181c5980a1d8de3f39ab784ac
samuelparada6 <samuelparada6@vt.edu>
2024-04-02 13:18:15 -0400

    added basic bottom sheet to PlanATrip tab

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,14 @@
 module.exports = function (api) {
-    api.cache(true);
+    api.cache.invalidate(() => process.env.GOOGLE_MAPS_API_KEY)
     return {
         presets: ['babel-preset-expo'],
         plugins: [
-            ['module:react-native-dotenv']
+            [
+                'module:react-native-dotenv',
+            ],
+            [
+                'react-native-reanimated/plugin',
+            ]
         ]
     };
 };
--- components/planTrip/PlanTrip.js
+++ components/planTrip/PlanTrip.js
@@ -1,10 +1,30 @@
-import { View, Text } from 'react-native';
-import styles from '../../styles/App.style'
+import { View, Text, StyleSheet} from 'react-native';
+import React, { useCallback, useMemo, useRef } from 'react';
+// import styles from '../../styles/App.style';
+import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
 
 export default function PlanTrip() {
+    const snapPoints = useMemo(() => ['25%', '50%', '70%'], []);
+
     return (
         <View style={styles.container}>
-            <Text>This is the Plan a Trip Tab</Text>
+            <BottomSheet snapPoints={snapPoints}>
+                <View>
+                    <Text>This is the Plan a Trip Tab</Text>
+                </View>
+            </BottomSheet>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: 'grey',
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
