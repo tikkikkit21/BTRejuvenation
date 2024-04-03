b7f6baf45e8e825934a384beda1e75b907692f81
tikkikkit21 <tikkikkit21@gmail.com>
2024-04-03 13:39:51 -0400

    Change storage key to global const

--- backend/userController.js
+++ backend/userController.js
@@ -1,9 +1,11 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+const FAVORITE_ROUTES_KEY = "favorite-routes";
+
 // sets the user's favorite routes, will overwrite existing
 export async function setFavoriteRoutes(favoriteRoutes) {
     try {
-        await AsyncStorage.setItem("favorite-routes", JSON.stringify(favoriteRoutes));
+        await AsyncStorage.setItem(FAVORITE_ROUTES_KEY, JSON.stringify(favoriteRoutes));
     } catch (error) {
         console.error(error);
     }
@@ -12,7 +14,7 @@ export async function setFavoriteRoutes(favoriteRoutes) {
 // fetches all the user's favorite routes, may be null
 export async function getFavoriteRoutes() {
     try {
-        const value = await AsyncStorage.getItem("favorite-routes");
+        const value = await AsyncStorage.getItem(FAVORITE_ROUTES_KEY);
         if (value !== null) {
             return JSON.parse(value);
         }
@@ -24,13 +26,13 @@ export async function getFavoriteRoutes() {
 
 export async function addFavoriteRoute(favoriteRoute) {
     try {
-        const value = await AsyncStorage.getItem("favorite-routes");
+        const value = await AsyncStorage.getItem(FAVORITE_ROUTES_KEY);
         const favoriteRoutes = value !== null
             ? favoriteRoutes = JSON.parse(value)
             : [];
 
         favoriteRoutes.append(favoriteRoute);
-        await AsyncStorage.setItem("favorite-routes", JSON.stringify(favoriteRoutes));
+        await AsyncStorage.setItem(FAVORITE_ROUTES_KEY, JSON.stringify(favoriteRoutes));
     } catch (error) {
         console.error(error);
     }
