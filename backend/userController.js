c42939c25e68d2eea69d17c4c70f7bf8ee3ae0b4
tikkikkit21 <tikkikkit21@gmail.com>
2024-04-03 13:55:14 -0400

    Add methods for favorite stops

--- backend/userController.js
+++ backend/userController.js
@@ -1,6 +1,9 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const FAVORITE_ROUTES_KEY = "favorite-routes";
+const FAVORITE_STOPS_KEY = "favorite-stops";
+
+/* Favorite routes */
 
 // sets the user's favorite routes, will overwrite existing
 export async function setFavoriteRoutes(favoriteRoutes) {
@@ -47,6 +50,59 @@ export async function deleteFavoriteRoute(favoriteRoute) {
             let favoriteRoutes = JSON.parse(value);
             favoriteRoutes = favoriteRoutes.filter(route => route !== favoriteRoute);
             await AsyncStorage.setItem(FAVORITE_ROUTES_KEY, JSON.stringify(favoriteRoutes));
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+/* Favorite Stops */
+
+// sets the user's favorite routes, will overwrite existing
+export async function setFavoriteStops(favoriteStops) {
+    try {
+        await AsyncStorage.setItem(FAVORITE_STOPS_KEY, JSON.stringify(favoriteStops));
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// fetches all the user's favorite stops, may be null
+export async function getFavoriteStops() {
+    try {
+        const value = await AsyncStorage.getItem(FAVORITE_STOPS_KEY);
+        if (value !== null) {
+            return JSON.parse(value);
+        }
+        return value;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// add a single stop to existing stops
+export async function addFavoriteStop(favoriteStop) {
+    try {
+        const value = await AsyncStorage.getItem(FAVORITE_STOPS_KEY);
+        const favoriteStops = value !== null
+            ? JSON.parse(value)
+            : [];
+
+        favoriteStops.push(favoriteStop);
+        await AsyncStorage.setItem(FAVORITE_STOPS_KEY, JSON.stringify(favoriteStops));
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// delete a single stop from existing stops
+export async function deleteFavoriteStop(favoriteStop) {
+    try {
+        const value = await AsyncStorage.getItem(FAVORITE_STOPS_KEY);
+        if (value !== null) {
+            let favoriteStops = JSON.parse(value);
+            favoriteStops = favoriteStops.filter(stop => stop !== favoriteStop);
+            await AsyncStorage.setItem(FAVORITE_STOPS_KEY, JSON.stringify(favoriteStops));
         }
     } catch (error) {
         console.error(error);
