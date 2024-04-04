89ce2378d243080fab0632de3179e68ecb576a0c
Samuel Parada <91508171+samuelparada6@users.noreply.github.com>
2024-04-04 15:31:08 -0400

    Added UI Components to Plan A Trip (#16)
    * added basic bottom sheet to PlanATrip tab
    
    * Added more styling to BottomSheet in Plan a Trip
    
    * added Maps to background of PlanATrip, added a textbox in PlanATrip bottomsheet
    
    * updated changes based on review comments

--- components/planTrip/PlanTrip.js
+++ components/planTrip/PlanTrip.js
@@ -1,10 +1,75 @@
-import { View, Text } from 'react-native';
-import styles from '../../styles/App.style'
+import { View, Text, StyleSheet } from 'react-native';
+import { Octicons } from '@expo/vector-icons';
+import React, { useMemo } from 'react';
+import BottomSheet, { BottomSheetTextInput } from '@gorhom/bottom-sheet';
+import Map from '../home/Map';
 
 export default function PlanTrip() {
+    // Points of the screen where the bottom sheet extends to
+    const snapPoints = useMemo(() => ['12%','25%', '50%', '70%', '95%'], []);
+
     return (
         <View style={styles.container}>
-            <Text>This is the Plan a Trip Tab</Text>
+          <MapViewMemo />
+          <BottomSheet 
+              snapPoints={snapPoints}
+              backgroundStyle={{backgroundColor: '#7F1237'}}
+          >
+              <View style={styles.inputContainer}>
+                  <Octicons name='search' size={15} color='#000'/>
+                  <BottomSheetTextInput style={styles.inputText} placeholder='Search for Destination'/>
+              </View>
+              <View style={[styles.inputContainer, styles.suggestedRoutesContainer]}>
+                <Text style={styles.suggestedRoutesText}>Suggested Routes</Text>
+              </View>
+          </BottomSheet>
         </View>
     );
 }
+
+// Memoized Map component to avoid unnecessary rerendering.
+const MapViewMemo = React.memo(Map);
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      backgroundColor: 'white',
+  },
+  containerHeadline: {
+      fontSize: 20,
+      fontWeight: '600',
+      padding: 20,
+      color: 'white',
+  },
+  inputContainer: {
+      flexDirection: 'row',
+      backgroundColor: 'white',
+      borderRadius: 10,
+      width: '90%',
+      paddingHorizontal: 10,
+      alignItems: 'center',
+      marginLeft: 20,
+  },
+  inputText: {
+      marginTop: 5,
+      marginHorizontal: 5,
+      marginBottom: 5,
+      borderRadius: 10,
+      fontSize: 16,
+      padding: 5,
+      backgroundColor: 'white',
+      color: 'black',
+  },
+  suggestedRoutesContainer: {
+      marginTop: 50,
+      backgroundColor: '#A24857',
+      paddingTop: 15,
+      paddingBottom: 70,
+  },
+  suggestedRoutesText: {
+      fontSize: 20,
+      fontWeight: '700',
+      color: 'white',
+      marginLeft: 5,
+  }
+});
