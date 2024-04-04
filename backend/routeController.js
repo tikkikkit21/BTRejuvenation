988543bb274d115b713fc71b6c0cb3a571958561
tikkikkit21 <tikkikkit21@gmail.com>
2024-04-04 15:23:59 -0400

    Clean up getScheduledRoutes

--- backend/routeController.js
+++ backend/routeController.js
@@ -11,19 +11,18 @@ const ROOT = "http://www.bt4uclassic.org/webservices/bt4u_webservice.asmx";
  */
 export async function getScheduledRoutes(stopCode) {
     // leaving servicedate as blank because it auto-searches for today's date
-    const { data } = await axios.get((`${ROOT}/GetScheduledRoutes?stopCode=${stopCode}&serviceDate=`));
-    json = xml2js(data, { compact: true });
-    scheduledStops = json.DocumentElement.ScheduledRoutes;
+    const { data } = await axios.get(`${ROOT}/GetScheduledRoutes?stopCode=${stopCode}&serviceDate=`);
+    const json = xml2js(data, { compact: true });
+    let scheduledStops = json.DocumentElement.ScheduledRoutes;
 
-    //when size is 1, data will be formatted as {}, when we need it as [] for mapping
-    if (Array.isArray(scheduledStops)) {
-        busRoutes = scheduledStops.map(route => [route.RouteName._text, route.RouteColor._text, route.RouteShortName._text, route.RouteTextColor._text, route.RouteURL._text, route.ServiceLevel._text]);
+    // when size is 1, data will be formatted as {}, when we need it as [] for mapping
+    if (!Array.isArray(scheduledStops)) {
+        scheduledStops = [scheduledStops]
     }
-    else {
-        busRoutes = [[scheduledStops.RouteName._text, scheduledStops.RouteColor._text, scheduledStops.RouteShortName._text, scheduledStops.RouteTextColor._text, scheduledStops.RouteURL._text, scheduledStops.ServiceLevel._text]];
-    }
+    
+    scheduledStops = scheduledStops.map(stop => formatStop(stop));
 
-    return busRoutes;
+    return scheduledStops;
 }
 
 /**
@@ -38,4 +37,12 @@ export async function getAllStops() {
     const stops = scheduledStops.map(stop => [stop.StopCode._text, stop.StopName._text]);
 
     return stops;
+}
+
+function formatStop(stopData) {
+    for (const key of Object.keys(stopData)) {
+        stopData[key] = stopData[key]._text;
+    }
+
+    return stopData;
 }
