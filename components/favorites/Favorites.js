aff91ba82a4ab2e88c15ad9ef33e4ab0c8094c2c
samuelparada6 <samuelparada6@vt.edu>
2024-04-09 14:42:38 -0400

    replaced Alerts w/ Favorites and rearranged tab order

--- components/favorites/Favorites.js
+++ components/favorites/Favorites.js
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native';
 import styles from '../../styles/App.style'
 
-export default function Alerts() {
+export default function Favorites() {
     return (
         <View style={styles.container}>
-            <Text>This is the Alerts Tab</Text>
+            <Text>This is the Favorites Tab</Text>
         </View>
     );
 }
