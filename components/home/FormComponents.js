56a77206313f616f1331155606a8d47357b7111b
tikkikkit21 <tikkikkit21@gmail.com>
2024-04-12 14:43:37 -0400

    Move text input code to component file

--- components/home/FormComponents.js
+++ components/home/FormComponents.js
@@ -1,9 +1,40 @@
 import React from "react";
 
-export function TextInput() {
-
+export function TextInput(question, placeholder, value) {
+    return (
+        <View style={styles.container} key={index}>
+            <Text style={styles.question}>{question}</Text>
+            <TextInput
+                style={styles.answer}
+                placeholder={placeholder}
+                value={value}
+            // onChangeText={text => item.question === "Full Name" ? setFullName(text) : setComments(text)}
+            />
+        </View>
+    );
 }
 
 export function SliderInput() {
 
 }
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        padding: 5,
+        margin: 5,
+        marginTop: 15
+    },
+    question: {
+        textAlign: 'left',  // Align questions on left side of screen
+        fontSize: 18,
+        marginBottom: 5,
+    },
+    answer: {
+        width: 325,
+        padding: 5,
+        borderColor: '#000',
+        borderWidth: 1,
+        borderRadius: 5
+    }
+});
