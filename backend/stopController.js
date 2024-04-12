74b16734a84cab8b2354231faebdf91b5848ee01
Tikki <35748816+tikkikkit21@users.noreply.github.com>
2024-04-11 20:01:48 -0400

    Clean up backend (#17)
    Added JSDoc comments to rest of backend functions and cleaned up some existing functions

--- backend/stopController.js
+++ backend/stopController.js
@@ -1,21 +1,20 @@
 import axios from "axios";
 import { xml2js } from "xml-js";
+import { formatTextProperty } from "./util";
 
 const ROOT = "http://www.bt4uclassic.org/webservices/bt4u_webservice.asmx";
 
-export async function getStops(busCode) {
-    const { data } = await axios.get(`${ROOT}/GetScheduledStopInfo?routeShortName=${busCode}&serviceDate=`);
+/**
+ * Fetches all stops for a route
+ * @param {string} routeCode route name abbreviation (ex: "HWA")
+ * @returns list of stops for that route
+ */
+export async function getStops(routeCode) {
+    const { data } = await axios.get(`${ROOT}/GetScheduledStopInfo?routeShortName=${routeCode}&serviceDate=`);
 
     let json = xml2js(data, { compact: true });
     json = json.DocumentElement.ScheduledStops;
-    json = json.map(stop => formatStop(stop));
+    json = json.map(stop => formatTextProperty(stop));
 
     return json;
-}
-
-function formatStop(stopData) {
-    for (const key of Object.keys(stopData)) {
-        stopData[key] = stopData[key]._text;
-    }
-    return stopData;
 }
