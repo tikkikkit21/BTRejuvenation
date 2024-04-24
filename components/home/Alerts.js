e5e77106ecf1fe0d1f950acb56172ff4e5c52827
tikkikkit21 <tikkikkit21@gmail.com>
2024-04-24 18:01:20 -0400

    Start styling alerts page

--- components/home/Alerts.js
+++ components/home/Alerts.js
@@ -1,6 +1,46 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { getAlerts } from '../../backend/alertController';
 
-export default function Alerts({ navigation }) {
-    return <Text>Alerts page</Text>;
+export default function Alerts() {
+    const [alerts, setAlerts] = useState([]);
+    useEffect(() => {
+        async function fetchAlerts() {
+            const alertsData = await getAlerts();
+            if (alertsData.length > 0) {
+                setAlerts(alertsData);
+            }
+        }
+        fetchAlerts();
+    }, []);
+
+    const alertViews = alerts.map(alert => {
+        return <View style={styles.alertSection}>
+            <Text style={styles.alertTitle}>{alert.AlertTitle}</Text>
+            <Text style={styles.alertDescription}>{alert.AlertMessage}</Text>
+        </View>;
+    });
+
+    return (
+        <View style={styles.container}>
+            {alertViews}
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        padding: 25
+    },
+    alertSection: {
+        margin: 3
+    },
+    alertTitle: {
+        fontSize: 20
+    },
+    alertDescription: {
+
+    }
+});
