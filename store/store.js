3ee058309d0cbd70ef609c512fdc42d87f72241c
tikkikkit21 <tikkikkit21@gmail.com>
2024-04-24 11:21:56 -0400

    Create reducer for refresh frequency

--- store/store.js
+++ store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import darkModeReducer from './darkModeReducer';
+import refreshFrequencyReducer from './refreshFrequencyReducer';
 
 export default configureStore({
     reducer: {
-        darkMode: darkModeReducer
+        darkMode: darkModeReducer,
+        refreshFrequency: refreshFrequencyReducer
     }
 });
