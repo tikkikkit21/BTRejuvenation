2eefe2647f1fbc4f43ef6964c11ce6f0a81def71
tikkikkit21 <tikkikkit21@gmail.com>
2024-04-24 19:11:03 -0400

    Finalize styling

--- components/home/Alerts.js
+++ components/home/Alerts.js
@@ -1,6 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import { getAlerts } from '../../backend/alertController';
+import { MaterialCommunityIcons, Foundation, AntDesign } from '@expo/vector-icons';
+
+
+const iconDict = {
+    "No Service": <Foundation name="prohibited" size={24} color="black" />,
+    "Reduced Service": <MaterialCommunityIcons name="bus-articulated-front" size={24} color="black" />,
+    "Significant Delays": <MaterialCommunityIcons name="bus-clock" size={24} color="black" />,
+    "Detour": <MaterialCommunityIcons name="transit-detour" size={24} color="black" />,
+    "Additional Service": <MaterialCommunityIcons name="bus-multiple" size={24} color="black" />,
+    "Modified Service": <MaterialCommunityIcons name="bus-marker" size={24} color="black" />,
+    "Stop Moved": <MaterialCommunityIcons name="bus-stop" size={24} color="black" />,
+    "Other": <MaterialCommunityIcons name="bus-alert" size={24} color="black" />,
+    "Unknown": <MaterialCommunityIcons name="alert" size={24} color="black" />
+}
 
 export default function Alerts() {
     const [alerts, setAlerts] = useState([]);
@@ -16,8 +30,11 @@ export default function Alerts() {
 
     const alertViews = alerts.map(alert => {
         return <View style={styles.alertSection}>
-            <Text style={styles.alertTitle}>{alert.AlertTitle}</Text>
-            <Text style={styles.alertDescription}>{alert.AlertMessage}</Text>
+            <View style={styles.alertText}>
+                {iconDict["Other"]}
+                <Text style={styles.alertTitle}>{alert.AlertTitle}</Text>
+            </View>
+            <AntDesign name="right" />
         </View>;
     });
 
@@ -34,16 +51,27 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        padding: 25
+        paddingHorizontal: 10
     },
     alertSection: {
-        margin: 3,
         padding: 5,
-        borderBottomColor: "black",
+        borderColor: "black",
         borderWidth: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    alertText: {
+        borderColor: "red",
+        borderWidth: 1,
+        flexDirection: "row",
+        alignItems: "center",
+        flex: 1
     },
     alertTitle: {
-        fontSize: 15
+        fontSize: 20,
+        flex: 1,
+        marginLeft: 5
     },
     alertDescription: {
         fontSize: 10
