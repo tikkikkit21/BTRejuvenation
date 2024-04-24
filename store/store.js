31776ad424a4c846ce02967d6557c7f902d8c30d
tikkikkit21 <tikkikkit21@gmail.com>
2024-04-24 09:10:55 -0400

    Add dark mode reducer to store

--- store/store.js
+++ store/store.js
@@ -1,5 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import darkModeReducer from './darkModeReducer';
 
 export default configureStore({
-    reducer: {}
+    reducer: {
+        darkMode: darkModeReducer
+    }
 });
