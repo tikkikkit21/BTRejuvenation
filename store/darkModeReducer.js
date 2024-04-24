10a5f911d75d703dd56eff2a3e44d5a0189b3788
tikkikkit21 <tikkikkit21@gmail.com>
2024-04-24 09:18:07 -0400

    Add thunk to handle fetching user setting

--- store/darkModeReducer.js
+++ store/darkModeReducer.js
@@ -1,17 +1,35 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { getDarkModeSetting } from "../backend/userController";
 
 const darkmodeSlice = createSlice({
     name: "darkmode",
     initialState: {
-        darkmode: false
+        darkmode: false,
+        loading: false
     },
     reducers: {
         setDarkMode(state, action) {
             console.log("action:", action);
             state.darkmode = action.payload
         }
+    },
+    extraReducers: builder => {
+        builder.addCase(getSetting.pending, state => {
+            state.loading = true
+        });
+        builder.addCase(getSetting.fulfilled, (state, action) => {
+            state.users = action.payload
+            state.loading = false
+        });
+        builder.addCase(getSetting.rejected, state => {
+            state.loading = false
+        });
     }
-})
+});
+
+const getSetting = createAsyncThunk('settings/darkMode', async () => {
+    return await getDarkModeSetting();
+});
 
 export const { setDarkMode } = darkmodeSlice.actions;
 export default darkmodeSlice.reducer;
