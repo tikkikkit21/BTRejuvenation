962999e3282d6df4054d12f85af7b4bbf602f103
samuelparada6 <samuelparada6@vt.edu>
2024-04-25 15:03:43 -0400

    nvm replaced Walking with Walk

--- components/navigate/Navigate.js
+++ components/navigate/Navigate.js
@@ -204,7 +204,7 @@ export default function Navigate() {
                     {routeData && (     // If routeData exists, display it
                         <View style={styles.routeOptionContainer}>
                             <RouteOption 
-                                busLine={routeData[0].mainBusLine === 'N/A' ? 'Walking' : routeData[0].mainBusLine}
+                                busLine={routeData[0].mainBusLine === 'N/A' ? 'Walk' : routeData[0].mainBusLine}
                                 tripDuration={routeData[0].totalDuration}
                                 tripDistance={routeData[0].totalDistance}
                                 routeColor={routeColor}
