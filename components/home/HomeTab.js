a4578a6fdf98bba977b913a61addd112ce500701
tikkikkit21 <tikkikkit21@gmail.com>
2024-04-27 14:10:47 -0400

    Move suggested route logic to HomeTab
    This is so we can handle navigation, which is not possible in App.js

--- components/home/HomeTab.js
+++ components/home/HomeTab.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useSelector } from 'react-redux';
+import * as Location from 'expo-location';
+import { getSuggestedRoute } from '../../backend/userController';
 import Map from './Map';
 import StopInfo from './StopInfo';
 import RouteInfo from '../routes/RouteInfo';
@@ -9,7 +13,55 @@ import Alerts from './Alerts';
 const Stack = createStackNavigator();
 
 function HomeTab() {
+    const navigation = useNavigation();
     const darkMode = useSelector(state => state.darkMode.isEnabled);
+
+    // suggested route alert
+    useEffect(() => {
+        async function fetchSuggestedRoute() {
+            // figure out suggested route
+            const location = await Location.getCurrentPositionAsync({});
+            const data = {
+                time: new Date(),
+                coords: {
+                    lat: location.coords.latitude,
+                    long: location.coords.longitude
+                }
+            };
+            const suggestedRoute = await getSuggestedRoute(data);
+
+            // if there's a valid suggested route, alert on initial startup
+            if (suggestedRoute) {
+                Alert.alert(
+                    "Suggested Route",
+                    suggestedRoute,
+                    [
+                        {
+                            text: "Sure",
+                            onPress: () => {
+                                console.log("navigating to routes list...");
+                                navigation.navigate(
+                                    "RoutesTab",
+                                    {
+                                        screen: "RouteInfo",
+                                        params: {
+                                            routeShortName: suggestedRoute,
+                                            routeName: "fullName",
+                                            routeColor: "red"
+                                        }
+                                    }
+                                );
+                            }
+                        },
+                        {
+                            text: "No thanks"
+                        }
+                    ]
+                );
+            }
+        }
+        fetchSuggestedRoute();
+    }, []);
 
     return (
         <Stack.Navigator
